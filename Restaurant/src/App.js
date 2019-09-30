34f6435786fc47d34702eced8dc6a57912128c46
Elkhan <elxanmursal@gmail.com>
2019-09-30 22:02:24 +0200

    Adding bootstrap tabs, dependant js libraries

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -4,24 +4,24 @@ import HeaderComponent from "./components/header";
 import MenuComponent from "./components/menu";
 import OrderComponent from "./components/order";
 
-const style = {
-  backgroundColor: "#2d39eb",
-  color: "white"
-};
+// const style = {
+//   backgroundColor: "#2d39eb",
+//   color: "white"
+// };
 
 function App() {
   return (
-    <main className="container-fluid d-flex flex-column h-100" style={style}>
+    <main className="container-fluid d-flex flex-column h-100">
       <div className="row">
         <section className="col-md-12 border border-primary">
           <HeaderComponent />
         </section>
       </div>
       <div className="row flex-fill">
-        <div className="col-md-9 border border-warning">
+        <div className="col-md-8 border border-warning">
           <MenuComponent name="Menu" />
         </div>
-        <div className="col-md-3 border border-success">
+        <div className="col-md-4 border border-success">
           <OrderComponent />
         </div>
       </div>
--- Restaurant/src/components/menu.jsx
+++ Restaurant/src/components/menu.jsx
@@ -1,29 +1,79 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const MenuComponent = props => {
-  const [textState] = useState({
-    text: "Hey, I'm"
-  });
-
-  const [orderState, orderSetState] = useState({
-    order: 0
-  });
-
-  const makeOrderHandler = () => {
-    orderSetState({ order: orderState.order + 1 });
-  };
-
-  return (
-    <React.Fragment>
-      <span>
-        {textState.text} {props.name}!
-      </span>
-      <span className="badge badge-info">{orderState.order}</span>
-      <button className="btn btn-success" onClick={makeOrderHandler}>
-        Make order
-      </button>
-    </React.Fragment>
-  );
-};
+class MenuComponent extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ul id="tabsMenu" className="nav nav-tabs" role="tablist">
+          <li className="nav-item">
+            <a
+              id="foodTab"
+              className="nav-link active"
+              data-toggle="tab"
+              href="#food"
+              role="tab"
+              aria-controls="food"
+              aria-selected="true"
+            >
+              Food
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              id="drinksTab"
+              className="nav-link"
+              data-toggle="tab"
+              href="#drinks"
+              role="tab"
+              aria-controls="drinks"
+              aria-selected="false"
+            >
+              Drinks
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              id="dessertTab"
+              className="nav-link"
+              data-toggle="tab"
+              href="#dessert"
+              role="tab"
+              aria-controls="dessert"
+              aria-selected="false"
+            >
+              Dessert
+            </a>
+          </li>
+        </ul>
+        <div id="menuContent" className="tab-content">
+          <div
+            id="food"
+            className="tab-pane fade show active"
+            role="tabpanel"
+            aria-labelledby="foodTab"
+          >
+            Foooooood
+          </div>
+          <div
+            id="drinks"
+            className="tab-pane fade"
+            role="tabpanel"
+            aria-labelledby="drinksTab"
+          >
+            Driiiiiinks
+          </div>
+          <div
+            id="dessert"
+            className="tab-pane fade"
+            role="tabpanel"
+            aria-labelledby="dessertTab"
+          >
+            Desseeeeeert
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MenuComponent;
