2323bbbade9f552e46987a5ab83c2c6e3661415e
Elkhan <elxanmursal@gmail.com>
2019-10-02 22:48:22 +0200

    Developing header compt

--- Restaurant/src/components/home/header.jsx
+++ Restaurant/src/components/home/header.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default class HeaderComponent extends Component {
   state = {
-    component: "Header"
+    title: "Restaurant",
+    userFullName: "Elkhan Mursali",
+    currentDateTime: "02-10-2019"
   };
 
   logOutHandler = () => {
@@ -14,10 +18,27 @@ export default class HeaderComponent extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <span>Hey, I'm {this.state.component}!</span>
-        <button onClick={this.logOutHandler}>Log out</button>
-      </React.Fragment>
+      <div className="row">
+        <div className="col-md-2">
+          <img
+            src="https://picsum.photos/75"
+            alt="avatar"
+            className="rounded mr-2"
+          />
+          <span className="font-weight-bold">{this.state.userFullName}</span>
+        </div>
+        <div className="col-md-7 p-3">
+          <h3 className="text-uppercase font-weight-bold text-center text-success">
+            {this.state.title}
+          </h3>
+        </div>
+        <div className="col-md-3 p-3 text-light">
+          <span className="bg-info mr-2 p-2">{this.state.currentDateTime}</span>
+          <a href="/" className="btn btn-success" click="onLock()">
+            <FontAwesomeIcon icon={faUnlockAlt} />
+          </a>
+        </div>
+      </div>
     );
   }
 }
